1315f2e5915d68a91761c9dc4a342d18d2e4d7b3
mys5284 <myx5284@naver.com>
2024-10-14 12:45:02 +0900

    10_14 UI

--- Assets/02. Scripts/Morgue/LeftController.cs
+++ Assets/02. Scripts/Morgue/LeftController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class LeftController : MonoBehaviour
+public class LeftController : XRRayInteractor
 {
     public InputActionReference menu; //메뉴 UI 켜는 키
     public InputActionReference collectionOn; //수집품 UI 켜는 키
@@ -13,6 +14,10 @@ public class LeftController : MonoBehaviour
     public GameObject UIcollection; //수집품 UI 오브젝트
 
     public GameObject LeftHandRender; //왼쪽 손 랜더러
+
+    public GameObject UIItem;
+    public Text UIItemText;
+
 
     private void Start()
     {
@@ -47,5 +52,100 @@ public class LeftController : MonoBehaviour
         LeftHandRender.SetActive(false);
     }
 
+    public bool isGrip = true;
+    protected override void OnSelectEntered(XRBaseInteractable interactable)
+    {
+        isGrip = false;
+    }
 
+    protected override void OnSelectExited(XRBaseInteractable interactable)
+    {
+        isGrip = true;
+    }
+
+    protected override void OnHoverEntered(XRBaseInteractable interactable)
+    {
+
+        //if (interactable.transform.childCount != 0)
+        //{
+        //    interactable?.transform.GetChild(0).gameObject.SetActive(true);
+        //    interactable.transform.GetChild(0).GetComponent<TextMesh>().text = "열기";
+        //}
+
+        if (isGrip)
+        {
+            switch (interactable.name)
+            {
+                case "Doll":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 인형";
+                    break;
+                case "Crowbar":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 크로우바";
+                    break;
+                case "MorgueKey":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 시체 안치실 열쇠";
+                    break;
+                case "DoctorCard":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 의사 카드";
+                    break;
+                case "CabinetKey":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 캐비넷 열쇠";
+                    break;
+                case "Pencil":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 연필";
+                    break;
+                case "OfficeKey":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 사무실 열쇠";
+                    break;
+                case "RemoCon":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 리모콘";
+                    break;
+                case "Flashlight":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 손전등";
+                    break;
+                case "Paper_None":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 빈(?)종이";
+                    break;
+                case "SurgeryKey":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 빈(?)종이";
+                    break;
+                case "Scalpel":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 메스";
+                    break;
+                case "PillowFabic_Medicine":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 깨끗한 천";
+                    break;
+                case "Medicine":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 약통";
+                    break;
+                case "PillowFabic":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 천";
+                    break;
+            }
+        }
+
+
+    }
+
+
+    protected override void OnHoverExited(XRBaseInteractable interactable)
+    {
+        UIItemText.text = "";
+        UIItem.SetActive(false);
+    }
 }
--- Assets/02. Scripts/Morgue/RightController.cs
+++ Assets/02. Scripts/Morgue/RightController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class RightController : MonoBehaviour
+public class RightController : XRRayInteractor
 {
     public GameObject RightHandRender;
+    public GameObject UIItem;
+    public Text UIItemText;
 
     public void RightHandRenderIdle()
     {
@@ -14,5 +18,128 @@ public class RightController : MonoBehaviour
     public void RightHandRenderGripping()
     {
         RightHandRender.SetActive(false);
+    }
+
+    //public void Entering(XRBaseInteractable interactable)
+    //{
+    //    if (isGrip)
+    //    {
+    //        switch (interactable.name)
+    //        {
+    //            case "Doll":
+    //                playerUI.SetActive(true);
+    //                playerUIText.text = "아이템: 인형";
+    //                break;
+    //            case "Crowbar":
+    //                playerUI.SetActive(true);
+    //                playerUIText.text = "아이템: 크로우바";
+    //                break;
+
+
+    //        }
+    //    }
+    //}
+
+    //public void Exited(XRBaseInteractable interactable)
+    //{
+    //    playerUIText.text = "";
+    //    playerUI.SetActive(false);
+    //}
+
+    public bool isGrip = true;
+    protected override void OnSelectEntered(XRBaseInteractable interactable)
+    {
+        isGrip = false;
+    }
+
+    protected override void OnSelectExited(XRBaseInteractable interactable)
+    {
+        isGrip = true;
+    }
+
+    protected override void OnHoverEntered(XRBaseInteractable interactable)
+    {
+
+        //if (interactable.transform.childCount != 0)
+        //{
+        //    interactable?.transform.GetChild(0).gameObject.SetActive(true);
+        //    interactable.transform.GetChild(0).GetComponent<TextMesh>().text = "열기";
+        //}
+
+        if (isGrip)
+        {
+            switch (interactable.name)
+            {
+                case "Doll":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 인형";
+                    break;
+                case "Crowbar":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 크로우바";
+                    break;
+                case "MorgueKey":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 시체 안치실 열쇠";
+                    break;
+                case "DoctorCard":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 의사 카드";
+                    break;
+                case "CabinetKey":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 캐비넷 열쇠";
+                    break;
+                case "Pencil":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 연필";
+                    break;
+                case "OfficeKey":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 사무실 열쇠";
+                    break;
+                case "RemoCon":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 리모콘";
+                    break;
+                case "Flashlight":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 손전등";
+                    break;
+                case "Paper_None":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 빈(?)종이";
+                    break;  
+                case "SurgeryKey":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 빈(?)종이";
+                    break;
+                case "Scalpel":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 메스";
+                    break; 
+                case "PillowFabic_Medicine":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 깨끗한 천";
+                    break; 
+                case "Medicine":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 약통";
+                    break; 
+                case "PillowFabic":
+                    UIItem.SetActive(true);
+                    UIItemText.text = "아이템: 천";
+                    break;
+            }
+        }
+
+
+    }
+
+
+    protected override void OnHoverExited(XRBaseInteractable interactable)
+    {
+        UIItemText.text = "";
+        UIItem.SetActive(false);
     }
 }

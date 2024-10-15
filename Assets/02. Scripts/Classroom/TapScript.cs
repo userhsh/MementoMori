99705336181009b676a0cee53940cef748cbe177
kim-han-woong <harumarry@naver.com>
2024-10-15 12:35:44 +0900

    classroomUpdate
    Sound & Etc

--- Assets/02. Scripts/Classroom/TapScript.cs
+++ Assets/02. Scripts/Classroom/TapScript.cs
@@ -5,17 +5,36 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class TapScript : MonoBehaviour
 {
+    private AudioClip waterSound;
+
+    private AudioSource audioSource;  // 오디오 소스
+    private XRGrabInteractable grabInteractable;
+
     bool isTurnOn = false;
     Animator animator;
 
     private void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
         animator = GetComponent<Animator>();
     }
 
-    public void TurnOnWater()
+    private void Start()
+    {
+        waterSound = Resources.Load<AudioClip>("WaterSound/waterSound");
+        // 물체를 잡을 때 호출되는 이벤트 연결
+        grabInteractable.selectEntered.AddListener(TurnOnWater);
+    }
+
+    public void TurnOnWater(SelectEnterEventArgs args)
     {
         Debug.Log("수도꼭지 틀기");
         animator.SetBool("isTurnOn", true);
+
+        audioSource.clip = waterSound;
+        audioSource.Play();
+        // 현재 오디오 시스템 시간에 2초 후에 종료하도록 예약
+        audioSource.SetScheduledEndTime(AudioSettings.dspTime + 2.5f);
     }
 }

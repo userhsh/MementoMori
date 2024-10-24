eb5a195d15e04338fbde545fabe37b66e5ef252a
kim-han-woong <harumarry@naver.com>
2024-10-24 15:44:45 +0900

    Master&SFX Volume

--- Assets/02. Scripts/GameStart/VolumeController.cs
+++ Assets/02. Scripts/GameStart/VolumeController.cs
@@ -1,36 +1,88 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class VolumeController : MonoBehaviour
 {
-    public Slider volumeSlider;   // 볼륨을 조절할 슬라이더
-    public AudioSource audioSource;  // 오디오 소스
+    public AudioMixer audioMixer;  // BackgroundMixer를 연결
+    public Slider bgmVolumeSlider;
+    public Slider masterVolumeSlider;  // 마스터 볼륨 슬라이더
+    public Slider sfxVolumeSlider;  // 효과음(SFX) 슬라이더
 
-    private void Start()
+    void Start()
     {
-        // PlayerPrefs에서 저장된 볼륨 값을 불러와 적용
-        if (PlayerPrefs.HasKey("Volume"))
+        // 씬 전환 후 슬라이더와 오디오 믹서의 값 동기화
+        SyncSlidersWithAudioMixer();
+
+        // 슬라이더 값 변경 시 볼륨 조정
+        bgmVolumeSlider.onValueChanged.AddListener(SetBGMVolume);
+        masterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
+        sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    void OnEnable()
+    {
+        // 씬 전환 후 슬라이더와 오디오 믹서 값 동기화 (활성화될 때)
+        SyncSlidersWithAudioMixer();
+    }
+
+    // 오디오 믹서와 슬라이더 값을 동기화하는 함수
+    void SyncSlidersWithAudioMixer()
         {
-            float savedVolume = PlayerPrefs.GetFloat("Volume");
-            volumeSlider.value = savedVolume;  // 슬라이더에 저장된 값 반영
-            audioSource.volume = savedVolume;  // 오디오 소스에 저장된 값 반영
+            float bgmVolume, masterVolume, sfxVolume;
+
+            // 오디오 믹서의 현재 볼륨 값을 받아와 슬라이더에 반영
+            audioMixer.GetFloat("BGM", out bgmVolume);
+            audioMixer.GetFloat("Master", out masterVolume);
+            audioMixer.GetFloat("SFX", out sfxVolume);
+
+            // 슬라이더에 오디오 믹서 값을 변환해서 반영 (dB를 0~1 범위로 변환)
+            bgmVolumeSlider.value = Mathf.Pow(10, bgmVolume / 20);
+            masterVolumeSlider.value = Mathf.Pow(10, masterVolume / 20);
+            sfxVolumeSlider.value = Mathf.Pow(10, sfxVolume / 20);
+        }
+
+        // 슬라이더 값이 변경될 때 호출될 함수
+        public void SetBGMVolume(float sliderValue)
+        {
+            // 슬라이더 값이 0일 경우 음소거 상태로 설정
+            if (sliderValue <= 0.0001f)  // 0에 너무 가까운 값을 방지하기 위해 작은 값을 설정
+            {
+                audioMixer.SetFloat("BGM", -80f);  // 최소 볼륨 (-80dB로 설정)
+            }
+            else
+            {
+                // 슬라이더 값을 -80dB에서 0dB 사이로 변환하여 오디오 믹서에 전달
+                float volume = Mathf.Log10(sliderValue) * 20;
+                audioMixer.SetFloat("BGM", volume);
+            }
+        }
+
+    // 마스터 볼륨 조정
+    public void SetMasterVolume(float sliderValue)
+    {
+        if (sliderValue <= 0.0001f)
+        {
+            audioMixer.SetFloat("Master", -80f);  // 최소 볼륨
         }
         else
         {
-            // 저장된 값이 없으면 기본 값 1로 설정
-            volumeSlider.value = 1;
-            audioSource.volume = 1;
+            float volume = Mathf.Log10(sliderValue) * 20;
+            audioMixer.SetFloat("Master", volume);
         }
-
-        // 슬라이더 값이 변경될 때 볼륨을 조절
-        volumeSlider.onValueChanged.AddListener(SetVolume);
     }
 
-    // 슬라이더 값을 기반으로 볼륨을 조절하는 함수
-    public void SetVolume(float volume)
+    // 효과음(SFX) 볼륨 조정
+    public void SetSFXVolume(float sliderValue)
     {
-        audioSource.volume = volume;  // 오디오 볼륨을 슬라이더 값으로 설정
-        PlayerPrefs.SetFloat("Volume", volume);  // 볼륨 값을 저장
-        PlayerPrefs.Save();  // PlayerPrefs 저장
+        if (sliderValue <= 0.0001f)
+        {
+            audioMixer.SetFloat("SFX", -80f);  // 최소 볼륨
+        }
+        else
+        {
+            float volume = Mathf.Log10(sliderValue) * 20;
+            audioMixer.SetFloat("SFX", volume);
+        }
     }
 }

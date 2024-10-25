a0407f1a0b7dfce1f44c05f71048ad6a46a9b2a9
kim-han-woong <harumarry@naver.com>
2024-10-25 16:56:20 +0900

    Fix sound bugs
    Maintains sound even when the game is shut down and running, correcting bugs where sound effects are affected by background sounds

--- Assets/02. Scripts/DataManagement/GameData.cs
+++ Assets/02. Scripts/DataManagement/GameData.cs
@@ -7,3 +7,11 @@ public class GameData
     public bool isContinueAvailable; // 이어하기 가능 여부를 포함
     // 다른 데이터도 필요하면 추가
 }
+
+[System.Serializable]
+public class VolumeData
+{
+    public float bgmVolume;
+    public float masterVolume;
+    public float sfxVolume;
+}

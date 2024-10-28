c43544b8abe51e98be1b57920b38b0f6442fcb0b
kim-han-woong <harumarry@naver.com>
2024-10-28 12:05:30 +0900

    Add the sound of rain to the classroom
    Additionally, I reduced the background sound volume, turned off the background sound in the main scene, and filled in the gaps between the classroom doors and walls.

---
+++ Assets/02. Scripts/Sound/SoundZone.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundZone : MonoBehaviour
+{
+    private AudioSource audioSource; // 소리를 재생할 AudioSource
+    public float fadeDuration = 1.0f; // 소리 페이드 아웃 시간
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();  
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PLAYER"))
+        {
+            audioSource.Play(); // 플레이어가 영역에 들어왔을 때
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("PLAYER"))
+        {
+            StartCoroutine(FadeOut(audioSource, fadeDuration)); // 플레이어가 영역을 벗어났을 때
+        }        
+    }
+
+    private IEnumerator FadeOut(AudioSource audioSource, float duration)
+    {
+        float startVolume = audioSource.volume; // 초기 볼륨
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            audioSource.volume = Mathf.Lerp(startVolume, 0, t / duration); // 볼륨을 서서히 줄임
+            yield return null; // 다음 프레임까지 대기
+        }
+        audioSource.Stop(); // 볼륨이 0이 되면 소리 정지
+        audioSource.volume = startVolume; // 볼륨을 초기값으로 복원
+    }
+}

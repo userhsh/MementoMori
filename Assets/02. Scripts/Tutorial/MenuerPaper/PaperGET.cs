e255dd544c1c64670e10390ac64b21f0d8b9bf32
kim-han-woong <harumarry@naver.com>
2024-11-06 09:28:45 +0900

    Add more etc.
    Add tutorial paper sound, normal ending scene audio, family photo audio

--- Assets/02. Scripts/Tutorial/MenuerPaper/PaperGET.cs
+++ Assets/02. Scripts/Tutorial/MenuerPaper/PaperGET.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class PaperGET : MonoBehaviour
 {
-   public TMenuerGetText tmenuerGetText;
+    public TMenuerGetText tmenuerGetText;
     public GameObject truePaper;
     public int pageNumber;
     public GameObject getPaperUI;
     public GameObject menuerEscButton;
+    private AudioSource audioSource;
+    private AudioClip PaperSFX;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        PaperSFX = Resources.Load<AudioClip>("TutorialSFX/PaperSFX");
+    }
 
     public void GetNumberPaper()
     {
@@ -18,7 +30,12 @@ public class PaperGET : MonoBehaviour
         truePaper.SetActive(false); //메뉴얼 이 페이지 해금
 
         tmenuerGetText.getSetPaperNumber = pageNumber; //페이지 넘버 T메뉴얼획득 텍스트 넘버링 보넴
-        this.gameObject.SetActive(false);
+        audioSource.PlayOneShot(PaperSFX);
+        Invoke("Deactivate", PaperSFX.length);
+    }
 
+    private void Deactivate()
+    {
+        this.gameObject.SetActive(false);
     }
 }
